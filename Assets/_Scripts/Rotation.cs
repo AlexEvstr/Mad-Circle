3d0ed78cc815886bb5fd16bebe0834de1bf70c09
Alexander Evstratov <alexdrugev@me.com>
2023-03-24 13:04:13 +0300

    Add score.

--- Assets/_Scripts/Rotation.cs
+++ Assets/_Scripts/Rotation.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Rotation : MonoBehaviour
 {
+    [SerializeField] private Text _scoreText;
+    private float _score;
     private float _speed = 150.0f;
 
     public bool isMove;
+
+    private void Start()
+    {
+        _score = 0;
+    }
 
     private void Update()
     {
@@ -13,11 +21,16 @@ public class Rotation : MonoBehaviour
         else
             transform.Rotate(0, 0, -_speed * Time.deltaTime);
         ChangeDirection();
+
+        _scoreText.text = _score.ToString();
     }
 
     public void ChangeDirection()
     {
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-        isMove = !isMove;
+        {
+            isMove = !isMove;
+            _score++;
+        }
     }
 }
